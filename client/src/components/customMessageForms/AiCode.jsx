a7f376776d03e391ab896c5959edf1baadbed6df
Jason Ngo <131301318+jasonsonthanhngo@users.noreply.github.com>
2023-07-19 16:30:49 -0700

    Add files via upload

--- client/src/components/customMessageForms/AiCode.jsx
+++ client/src/components/customMessageForms/AiCode.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-
 import MessageFormUI from './MessageFormUI';
+import { usePostAiCodeMutation } from '@/state/api';
 
-const StandardMessageForm = ({ props, activeChat }) => {
+const AiCode = ({ props, activeChat }) => {
 	const [message, setMessage] = useState('');
 	const [attachment, setAttachment] = useState('');
+	const [triggerCode] = usePostAiCodeMutation();
 
 	const handleChange = (e) => setMessage(e.target.value);
 
@@ -23,10 +24,10 @@ const StandardMessageForm = ({ props, activeChat }) => {
 		};
 
 		props.onSubmit(form);
+		triggerCode(form);
 		setMessage('');
 		setAttachment('');
 	};
-
 	return (
 		<MessageFormUI
 			setAttachment={setAttachment}
@@ -37,4 +38,4 @@ const StandardMessageForm = ({ props, activeChat }) => {
 	);
 };
 
-export default StandardMessageForm;
+export default AiCode;

77d7031f93152e253c52cd768f6fd6f5b935f7dd
Jason Ngo <131301318+jasonsonthanhngo@users.noreply.github.com>
2023-07-19 23:29:17 -0700

    Add files via upload

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -68,4 +68,22 @@ router.post('/code', async (req, res) => {
 		res.status(500).json({ error: error.message });
 	}
 });
+router.post('/assist', async (req, res) => {
+	try {
+		const { text } = req.body;
+		const response = await openai.createCompletion({
+			model: 'text-davinci-003',
+			prompt: `Finish my thought: ${text}`,
+			temperature: 0.5,
+			max_tokens: 1024,
+			top_p: 1,
+			frequency_penalty: 0.5,
+			presence_penalty: 0,
+		});
+		res.status(200).json({ text: response.data.choices[0].text });
+	} catch (error) {
+		console.error('error', error.response.data.error);
+		res.status(500).json({ error: error.message });
+	}
+});
 export default router;
